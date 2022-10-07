6a41b78e3f5c50e68108c2e6bdd13480acb65843
Doggoro <99811661+Chih-Ling-Wu@users.noreply.github.com>
2022-10-07 22:36:46 +0800

    Create Q5.m

---
+++ Q5.m
@@ -0,0 +1,10 @@
+clc
+clear all
+subplot (1,2,1)
+fplot (@(x) -sqrt(cos(x)) + 3, [-pi/2, pi/2])
+title('sqrt(cos(x))+3');
+xlabel('x');
+
+subplot(1,2,2)
+syms x y
+ezplot((x.^2/4 - y.^2/16), [-2,2, -4,4])
