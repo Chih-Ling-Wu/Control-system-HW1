3fe9a75336ccb26b07d32ae75f24d47e302250a2
Doggoro <99811661+Chih-Ling-Wu@users.noreply.github.com>
2022-10-12 00:09:50 +0800

    Update Q5.m

--- Q5.m
+++ Q5.m
@@ -6,11 +6,11 @@ title('-sqrt(cos(x))+3');
 xlabel('x');
 
 subplot(1,2,2)
-X = [-2:0.01:2]
-Y = [-4:0.01:4]
-[x, y] = meshgrid(X, Y)
-f = (x.^2/(2^2)) - (y.^2/(4^2))
-contour(x,y,f)
-title('x^2/(2^2) - y^2/(4^2)');
+X = [-2:0.01:2];
+Y = [-4:0.01:4];
+[x, y] = meshgrid(X, Y);
+f = (x.^2/(2^2)) - (y.^2/(4^2));
+contour(x,y,f);
+title('x^2/(2^2) - y^2/(4^2)')
 xlabel('x');
 ylabel('y');
